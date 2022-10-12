542096df5d756394bfa1660c754cbfc4ffb69912
crayolito <josalejandroapp2017@gmail.com>
2022-10-12 10:50:12 -0400

    sql

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,12 @@
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_BASE, DB_PORT} = require('./lib/config')
+const {PORT,DB_HOST, DB_USER, DB_PASSWORD, DB_BASE, DB_PORT} = require('./lib/config')
 
 module.exports = {
     database: {
         host: DB_HOST,
         user: DB_USER,
+        post: DB_PORT,
         password: DB_PASSWORD,
         database: DB_BASE,
-        port : DB_PORT
     }
+
 }
