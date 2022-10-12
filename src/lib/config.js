898a9edeebbd2265900c7505697f0a1658a43fa7
crayolito <josalejandroapp2017@gmail.com>
2022-10-12 12:10:00 -0400

    sql

--- src/lib/config.js
+++ src/lib/config.js
@@ -9,12 +9,12 @@ config()
  const AWS_SECRET_KEY_S3 = process.env.AWS_SECRET_KEY_S3
  const AWS_PUBLIC_KEY_IA = process.env.AWS_PUBLIC_KEY_IA
  const AWS_SECRET_KEY_IA = process.env.AWS_SECRET_KEY_IA
- const PORT = process.env.PORT
- const DB_HOST = process.env.DB_HOST
- const DB_PORT = process.env.DB_PORT
- const DB_USER = process.env.DB_USER
- const DB_PASSWORD = process.env.DB_PASSWORD
- const DB_BASE = process.env.DB_BASE
+ const PORT = process.env.PORT || 5001
+ const DB_HOST = process.env.DB_HOST || 'localhost'
+ const DB_PORT = process.env.DB_PORT || 3306
+ const DB_USER = process.env.DB_USER   || 'root'
+ const DB_PASSWORD = process.env.DB_PASSWORD || 'coraxbay78452415'
+ const DB_BASE = process.env.DB_BASE || 'is1'
 module.exports = {
     AWS_BUCKET_NAME,
     AWS_BUCKET_REGION,
